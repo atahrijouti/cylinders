c462f996a0e436a6dc58515f952ee18692471fa5
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2020-07-02 16:41:24 +0200

    achieve some sense with rotating cube colors and lights

--- src/wheels.ts
+++ src/wheels.ts
@@ -1,39 +1,59 @@
 import {
+  AmbientLight,
   BoxGeometry,
+  Color,
+  DirectionalLight,
   Mesh,
-  MeshBasicMaterial,
+  MeshPhongMaterial,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
+  MathUtils,
 } from "three"
 import "./wheels.css"
+
+const { degToRad: d2r } = MathUtils
 
 const WIDTH = 800
 const HEIGHT = 400
 
 const scene = new Scene()
-const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000)
+scene.background = new Color(0xdedede)
 
 const renderer = new WebGLRenderer()
 renderer.setSize(WIDTH, HEIGHT)
 document.getElementById("root")?.appendChild(renderer.domElement)
 
-const geometry = new BoxGeometry()
-const material = new MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new Mesh(geometry, material)
+const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 2000)
+camera.position.set(1.024, 2.459, 3.0)
+camera.rotation.set(d2r(-22.74), d2r(18.59), d2r(0))
+
+const cube = new Mesh(
+  new BoxGeometry(),
+  new MeshPhongMaterial({
+    color: 0x00ff4c,
+    emissive: 0x000000,
+    specular: 0x111111,
+    shininess: 30,
+  })
+)
+cube.position.set(0, 1, 0)
+cube.rotation.set(d2r(3.5), d2r(20.97), d2r(21.15))
 scene.add(cube)
 
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
+const ambient = new AmbientLight(0xffe380, 0.15)
+scene.add(ambient)
 
-camera.lookAt(cube.position)
+const redLight = new DirectionalLight(0xffffff, 1)
+redLight.position.set(10, 10, 5)
+redLight.lookAt(cube.position)
+scene.add(redLight)
 
 const animate = function () {
   requestAnimationFrame(animate)
 
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
+  cube.rotation.x += 0.025
+  cube.rotation.y += 0.015
 
   renderer.render(scene, camera)
 }
