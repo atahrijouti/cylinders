8cdb98b149779b4df5c322840a00efc402909bbb
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2020-07-03 17:34:17 +0200

    use axes helper, add cylinders and fiddle with controls

--- src/wheels.ts
+++ src/wheels.ts
@@ -14,6 +14,10 @@ import {
   DodecahedronGeometry,
   Vector3,
   ArrowHelper,
+  AxesHelper,
+  CylinderGeometry,
+  PointLightHelper,
+  DirectionalLightHelper,
 } from "three"
 import "./wheels.css"
 import degToRad = MathUtils.degToRad
@@ -55,18 +59,11 @@ function mainScreen(renderer: Renderer) {
   const scene = new Scene()
   scene.background = new Color(0xdedede)
 
-  const subject = new Mesh(
-    new DodecahedronGeometry(),
-    new MeshPhongMaterial({
-      color: 0x739a73,
-      emissive: 0x000000,
-      specular: 0x111111,
-      shininess: 30,
-    })
-  )
-  subject.position.set(0, 0, 0)
-  // subject.rotation.set(d2r(3.5), d2r(20.97), d2r(21.15))
-  scene.add(subject)
+  const subjects = Array.from({ length: 12 }, (_, i) => createCylinder(i + 2))
+  subjects.forEach((subject, i) => {
+    subject.position.x = i + 0.5 + 0.5 * i
+    scene.add(subject)
+  })
 
   const ambient = new AmbientLight(0xfffffff, 0.15)
   scene.add(ambient)
@@ -74,30 +71,35 @@ function mainScreen(renderer: Renderer) {
   const white = new DirectionalLight(0xffffff, 1)
   white.position.set(5, 5, 5)
   scene.add(white)
+  scene.add(new DirectionalLightHelper(white))
 
   const red = new PointLight(0xff0000, 1)
   red.position.set(-5, 5, 0)
   scene.add(red)
+  scene.add(new PointLightHelper(red, 1))
 
   const blue = new PointLight(0x11abff, 1)
-  blue.position.set(5, -5, 0)
+  blue.position.set(5, 3, 3)
   scene.add(blue)
+  scene.add(new PointLightHelper(blue, 1))
 
   const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 2000)
-  camera.position.set(0, 5, 15.5)
-  // camera.rotation.set(d2r(1), d2r(0), d2r(0))
-  camera.lookAt(subject.position)
+  camera.position.set(-1, 3, 3)
+  camera.rotation.set(degToRad(-190), degToRad(0), degToRad(0))
+  // camera.lookAt(new Vector3(0, 0, 0))
 
   const controls = new OrbitControls(camera, renderer.domElement)
-  scene.add(new ArrowHelper(new Vector3(1, 0, 0), new Vector3(0, 0, 0), 10, 0xff0000, 0.5, 0.3))
-  scene.add(new ArrowHelper(new Vector3(0, 1, 0), new Vector3(0, 0, 0), 10, 0x00ff00, 0.5, 0.3))
-  scene.add(new ArrowHelper(new Vector3(0, 0, 1), new Vector3(0, 0, 0), 10, 0x0000ff, 0.5, 0.3))
-
+  controls.autoRotate = false
+  // scene.add(new ArrowHelper(new Vector3(1, 0, 0), new Vector3(0, 0, 0), 10, 0xff0000, 0.5, 0.3))
+  // scene.add(new ArrowHelper(new Vector3(0, 1, 0), new Vector3(0, 0, 0), 10, 0x00ff00, 0.5, 0.3))
+  // scene.add(new ArrowHelper(new Vector3(0, 0, 1), new Vector3(0, 0, 0), 10, 0x0000ff, 0.5, 0.3))
+  scene.add(new AxesHelper(10))
   function update(dt: number) {
     controls.update()
 
-    subject.rotation.x += degToRad(15) * dt
-    subject.rotation.y += degToRad(15) * dt
+    // subject.position.x += 3 * dt
+    subjects.forEach((subject) => (subject.rotation.x += degToRad(15) * dt))
+    // subject.rotation.y += degToRad(15) * dt
   }
 
   return {
@@ -105,4 +107,19 @@ function mainScreen(renderer: Renderer) {
     scene,
     camera,
   }
+}
+
+function createCylinder(segments: number) {
+  const mesh = new Mesh(
+    new CylinderGeometry(1, 1, 1, segments),
+    new MeshPhongMaterial({
+      color: 0x739a73,
+      emissive: 0x000000,
+      specular: 0x111111,
+      shininess: 30,
+    })
+  )
+  mesh.position.set(0, 0, 0)
+  mesh.rotation.set(degToRad(0), degToRad(0), degToRad(90))
+  return mesh
 }
