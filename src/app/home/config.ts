5b65f1e58b19db70a10a93619ba6dffdf67d994b
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2025-07-28 15:08:22 +0200

    less is more

--- src/app/home/config.ts
+++ src/app/home/config.ts
@@ -1,9 +1,9 @@
 export const WIDTH = 800
 export const HEIGHT = 800
 
-export const MAX_CYLINDER_COUNT = 20
+export const MAX_CYLINDER_COUNT = 16
 export const CYLINDER_SCALE = 0.5
-export const CARPET_LENGTH = 10
+export const CARPET_LENGTH = 4
 export const OPAQUE_COLOR = 0x000010
 export const OPAQUE_SHAPE_DOWN_SCALE = 0.9899
 export const LINE_COLOR = 0x04d9ff
